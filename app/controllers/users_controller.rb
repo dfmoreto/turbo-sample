ada149e115d95eb7ab60df806b04895f8ea31a37
Daniel Moreto <dfmoreto@gmail.com>
2021-08-05 20:03:38 -0300

    Sample with Turbo

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  # GET /users or /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1 or /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+    @user.save!
+  rescue ActiveRecord::RecordInvalid => e
+    render :new
+  end
+
+  # PATCH/PUT /users/1 or /users/1.json
+  def update
+    @user.update!(user_params)
+  rescue ActiveRecord::RecordInvalid => e
+    render :edit
+  end
+
+  # DELETE /users/1 or /users/1.json
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+end
